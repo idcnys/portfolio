d32461241c16112eedb496607445bad0410979da
idcnys <i.bittosaha@gmail.com>
2026-04-07 21:32:50 +0600

    Allow Cal and Firebase scripts in CSP

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,12 @@ const nextConfig: NextConfig = {
   async headers() {
     const csp = [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://app.cal.com https://*.firebaseio.com",
+      "script-src-elem 'self' 'unsafe-inline' https://app.cal.com https://*.firebaseio.com",
       "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com",
       "img-src 'self' data: blob: https:",
       "font-src 'self' data: https://cdnjs.cloudflare.com",
-      "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.firebaseapp.com https://*.gstatic.com https://codeforces.com https://alfa-leetcode-api.onrender.com https://tryhackme.com https://api.github.com https://img.shields.io",
+      "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com https://*.firebaseapp.com https://*.gstatic.com https://app.cal.com https://codeforces.com https://alfa-leetcode-api.onrender.com https://tryhackme.com https://api.github.com https://img.shields.io",
       "frame-src 'self' https:",
       "object-src 'none'",
       "base-uri 'self'",
