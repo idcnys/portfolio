364280d67e7ef342fdf2c3158f7f14d9cda7f626
= <i.bittosaha@gmail.com>
2026-05-07 15:46:55 +0600

    Fixed vercel version conflicts

---
+++ open-next.config.ts
@@ -0,0 +1,5 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+  // Keep defaults; this file enables OpenNext for Cloudflare Pages.
+});
